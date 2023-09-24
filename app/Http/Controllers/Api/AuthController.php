0e41d15085fd8a58baf094e4ecc09e0e90cfafd9
jendrabn <jendra455@gmail.com>
2023-09-24 19:55:41 +0700

    [REFACTOR] POST /api/auth/register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,34 +1,24 @@
 <?php
 
+// app/Http/Controllers/Api/AuthController.php
+
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
   public function register(RegisterRequest $request): JsonResponse
   {
-    $validatedData = $request->validated();
-    $validatedData['password'] = Hash::make($validatedData['password']);
+    $user = User::create($request->validated())
+      ->assignRole('user');
 
-    $user = User::create($validatedData);
-
-    $user->assignRole('user');
-
-    return response()->json([
-      'data' => [
-        'id' => $user->id,
-        'name' => $user->name,
-        'email' => $user->email,
-        'phone' => $user->phone,
-        'sex' => $user->sex,
-        'birth_date' => $user->birth_date,
-      ]
-    ], 201);
+    return (new UserResource($user))->response()->setStatusCode(Response::HTTP_CREATED);
   }
 }
