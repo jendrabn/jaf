a4a9c10e7ba8a9003d5ab9e235f724a4a4f2eebb
jendrabn <jendra455@gmail.com>
2023-09-24 19:35:17 +0700

    [TEST] GET /api/auth/register

---
+++ tests/Feature/Api/AuthRegisterPostTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Requests\Api\RegisterRequest;
+use App\Models\User;
+use Database\Seeders\RolesAndPermissionsSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+use Tests\TestCase;
+
+class AuthRegisterPostTest extends TestCase
+{
+  use RefreshDatabase;
+
+  /** @test */
+  public function register_uses_the_correct_form_request()
+  {
+    $this->assertActionUsesFormRequest(AuthController::class, 'register', RegisterRequest::class);
+  }
+
+  /** @test */
+  public function register_request_has_the_correct_rules()
+  {
+    $this->assertValidationRules(
+      [
+        'name' => [
+          'required',
+          'string',
+          'min:1',
+          'max:30',
+        ],
+        'email' => [
+          'required',
+          'string',
+          'email',
+          'min:1',
+          'max:255',
+          Rule::unique('users', 'email')
+        ],
+        'password' => [
+          'required',
+          'string', Password::min(8)->mixedCase()->numbers(),
+          'max:30',
+          'confirmed'
+        ],
+      ],
+      (new RegisterRequest())->rules()
+    );
+  }
+
+  /** @test */
+  public function can_register()
+  {
+    $this->seed(RolesAndPermissionsSeeder::class);
+    $data = [
+      'name' => 'John Cena',
+      'email' => 'jcena@gmail.com',
+      'password' => 'seCret123',
+      'password_confirmation' => 'seCret123'
+    ];
+
+    $response = $this->postJson('/api/auth/register', $data);
+
+    $response->assertCreated()
+      ->assertExactJson(['data' => [
+        'id' => 1,
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'phone' => null,
+        'sex' => null,
+        'birth_date' => null,
+      ]]);
+
+    $this->assertDatabaseCount('users', 1)
+      ->assertDatabaseHas('users', Arr::only($data, ['name', 'email']));
+
+    $user = User::whereEmail($data['email'])->first();
+
+    $this->assertTrue(Hash::check($data['password'], $user->password));
+    $this->assertTrue($user->hasRole('user'));
+  }
+
+  /** @test */
+  public function returns_validation_error_if_all_fields_are_invalid()
+  {
+    $data = [
+      'name' => '',
+      'email' => 'jcenagmail.com',
+      'password' => 'seCret123',
+      'password_confirmation' => 'seCret'
+    ];
+
+    $response = $this->postJson('/api/auth/register', $data);
+
+    $response->assertUnprocessable()
+      ->assertJsonValidationErrors(['name', 'email', 'password']);
+  }
+}
