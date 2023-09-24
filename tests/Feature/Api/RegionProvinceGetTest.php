4eddba7ac43d4dbaba1d893ff142d7b5d06e5aee
jendrabn <jendra455@gmail.com>
2023-09-24 11:46:00 +0700

    [TEST] GET /api/region/provinces

---
+++ tests/Feature/Api/RegionProvinceGetTest.php
@@ -0,0 +1,33 @@
+<?php
+
+// tests/Feature/Api/RegionProvinceGetTest.php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Province;
+use Database\Seeders\ProvinceSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegionProvinceGetTest extends TestCase
+{
+  use RefreshDatabase;
+
+  /** @test */
+  public function can_get_all_provinces()
+  {
+    $this->seed(ProvinceSeeder::class);
+
+    $provinces = Province::all()
+      ->map(fn ($province) => $province->only(['id', 'name']))
+      ->toArray();
+
+    $response = $this->getJson('/api/region/provinces');
+
+    $response->assertOk()
+      ->assertJsonStructure(['data' => ['*' => ['id', 'name']]])
+      ->assertJsonCount(34, 'data')
+      ->assertJson(['data' => $provinces]);
+  }
+}
