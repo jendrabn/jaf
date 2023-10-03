3a7a7ce1e1763dcb5e553544adeb9a5a29987dc8
jendrabn <jendra455@gmail.com>
2023-10-03 14:45:58 +0700

    [REFACTOR] GET /api/home_page

--- app/Http/Controllers/Api/HomePageController.php
+++ app/Http/Controllers/Api/HomePageController.php
@@ -3,29 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BannerResource;
 use App\Http\Resources\ProductResource;
 use App\Models\Banner;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomePageController extends Controller
 {
   public function __invoke(): JsonResponse
   {
-    $banners = Banner::orderBy('id')->take(10)->get();
+    $banners = Banner::take(10)->get();
     $products = Product::published()->latest('id')->take(10)->get();
 
-    return response()->json([
-      'data' => [
-        'banners' => $banners->map(fn ($banner) => [
-          'id' => $banner->id,
-          'image' => $banner->image,
-          'image_alt' => $banner->image_alt,
-          'url' => $banner->url,
-        ])->toArray(),
-        'products' => ProductResource::collection($products)
-      ]
-    ]);
+    return response()
+      ->json([
+        'data' => [
+          'banners' => BannerResource::collection($banners),
+          'products' => ProductResource::collection($products),
+        ]
+      ])
+      ->setStatusCode(Response::HTTP_OK);
   }
 }
---
+++ app/Http/Resources/BannerResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BannerResource extends JsonResource
+{
+  /**
+   * Transform the resource into an array.
+   *
+   * @return array<string, mixed>
+   */
+  public function toArray(Request $request): array
+  {
+    return [
+      'id' => $this->id,
+      'image' => $this->image,
+      'image_alt' => $this->image_alt,
+      'url' => $this->url,
+    ];
+  }
+}
