c73dbabc374a27f5ffa98e7cdf566c713fc6a46a
jendrabn <jendra455@gmail.com>
2023-10-05 21:39:33 +0700

    [TEST]  Cancel expired order

---
+++ tests/Feature/CancelExpiredOrderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Invoice;
+use App\Models\Order;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CancelExpiredOrderTest extends TestCase
+{
+  use RefreshDatabase;
+
+  /** @test */
+  public function can_cancel_expired_order()
+  {
+    $user = $this->createUser();
+    $order = Order::factory()
+      ->for($user)
+      ->afterCreating(
+        fn (Order $order) => Invoice::factory()
+          ->create(['due_date' => $order->created_at->addDays(1)])
+      )
+      ->create(['status' => Order::STATUS_PENDING_PAYMENT]);
+
+    $this->travel(25)->hours();
+
+    $this->artisan('app:cancel-expired-order');
+
+    $this->assertTrue($order->fresh()->status === Order::STATUS_CANCELLED);
+  }
+}
