7f6a39c5b21d9bbaa0ebfebeab04907680fc6365
jendrabn <jendra455@gmail.com>
2023-10-06 21:14:08 +0700

    [REFACTOR] POST /api/carts

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -4,45 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\CreateCartRequest;
-use App\Models\Cart;
-use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
+use App\Services\CartService;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends Controller
 {
-  public function create(CreateCartRequest $request)
+  public function create(CreateCartRequest $request, CartService $cartService): JsonResponse
   {
-    $product = Product::where('is_publish', true)
-      ->where('id', $request->validated('product_id'))
-      ->first();
+    $validatedData = $request->validated();
+    $user = auth()->user();
 
-    if ($product === null) {
-      throw ValidationException::withMessages([
-        'product' => 'Produk tidak tersedia'
-      ]);
-    }
+    $cartService->addToCart(
+      $validatedData['product_id'],
+      $validatedData['quantity'],
+      $user
+    );
 
-    $cart = Cart::where('product_id', $request->validated('product_id'))
-      ->first();
-
-    if ($request->validated('quantity') + ($cart['quantity'] ?? 0) > $product['stock']) {
-      throw ValidationException::withMessages([
-        'cart' => 'Kuantitas melebihi stok yang tersedia.'
-      ]);
-    }
-
-    if ($cart === null) {
-      Cart::create([
-        'user_id' => auth()->id(),
-        'product_id' => $request->validated('product_id'),
-        'quantity' => $request->validated('quantity'),
-      ]);
-    } else {
-      $cart->update([
-        'quantity' => $request->validated('quantity') + $cart['quantity'],
-      ]);
-    }
-    return response()->json(['data' => true])->setStatusCode(201);
+    return response()->json(['data' => true], Response::HTTP_CREATED);
   }
 }
--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -3,11 +3,37 @@
 namespace App\Services;
 
 use App\Models\Cart;
+use App\Models\Product;
 use App\Models\Shipping;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Validation\ValidationException;
 
 class CartService
 {
+  public function addToCart(int $productId, int $quantity, User $user): Cart
+  {
+    $product = Product::published()->find($productId);
+
+    if (!$product)
+      throw ValidationException::withMessages([
+        'product' => 'Produk tidak tersedia.'
+      ]);
+
+    $cart = $user->carts()->firstOrNew(['product_id' => $productId]);
+    $newQuantity = $cart->quantity + $quantity;
+
+    if ($newQuantity > $product->stock)
+      throw ValidationException::withMessages([
+        'cart' => 'Kuantitas melebihi stok yang tersedia.'
+      ]);
+
+    $cart->quantity = $newQuantity;
+    $cart->save();
+
+    return $cart;
+  }
+
   public function getTotalQuantity(Collection $carts): int
   {
     return $carts->reduce(
