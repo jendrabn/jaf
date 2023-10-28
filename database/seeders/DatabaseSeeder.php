26f1d6114fde4ad6aedafc24e2693d94118c8344
jendrabn <jendra455@gmail.com>
2023-10-28 17:18:21 +0700

    Add Laravel UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,43 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    // \App\Models\User::factory()->create([
+    //     'name' => 'Test User',
+    //     'email' => 'test@example.com',
+    // ]);
+
+    $this->call([
+      RolesAndPermissionsSeeder::class,
+      ProvinceSeeder::class,
+      CitySeeder::class,
+      ProductCategorySeeder::class,
+      ProductBrandSeeder::class,
+      BankSeeder::class
+    ]);
+
+    User::create([
+      'name' => 'Admin',
+      'email' => 'admin@mail.com',
+      'password' => 'Secret123',
+    ])->assignRole(User::ROLE_ADMIN);
+
+    User::create([
+      'name' => 'User',
+      'email' => 'user@mail.com',
+      'password' => 'Secret123',
+    ])->assignRole(User::ROLE_USER);
+  }
 }
